07dc3980b8590fb11030fd7429d86349badeaada
Vikash <vikash2000158@gmail.com>
2023-07-19 13:33:04 +0530

    some change

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,13 +4,9 @@ import './globals.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from "react";
 import Header from '@/components/layout/Header'
-import Head from 'next/head';
 
-export const metadata = {
-  title: 'Lasles VPN',
-  description: 'VPN stands for "Virtual Private Network" and describes the opportunity to establish a protected network connection when using public networks. VPNs encrypt your internet traffic and disguise your online identity. This makes it more difficult for third parties to track your activities online and steal data. The encryption takes place in real time.',
 
-}
+
 
 export default function RootLayout({ children }) {
 
